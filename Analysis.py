338dca2fc6311de288229b9dc6ed3854aa26ad1a
Gaurav <24311a67a0@ds.sreenidhi.edu.in>
2026-01-16 18:37:12 +0530

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,14 @@
-print("This is Testing")
+import pandas as pd
+
+df = pd.read_csv("DataSets_Loaded/countries.csv")
+
+df.columns = df.columns.str.strip().str.lower()
+
+country = input("Enter country name: ").strip().lower()
+
+result = df[df['country'].str.lower() == country]
+
+if result.empty:
+    print("Country not found")
+else:
+    print("Climate:", result.iloc[0]['Temperature'])
