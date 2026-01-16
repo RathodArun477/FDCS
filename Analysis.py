f8db109953e49264e316b77619734163430580f2
mdyasar120-dot <mdyasar120@gmail.com>
2026-01-16 14:46:25 +0530

    updated

--- Analysis.py
+++ Analysis.py
@@ -3,18 +3,18 @@ import pandas as pd
 print("Starting data cleaning...")
 countries = pd.read_csv("DataSets_Raw/Climate_1.csv")
 
-print("Raw country columns:")
+print("Raw country columns:-")
 print(countries.columns)
 
 countries_clean = countries[["Country", "ISO3", "Temperature"]]
 countries_clean["Country"] = countries_clean["Country"].str.strip()
 countries_clean.to_csv("DataSets_Loaded/countries.csv", index=False)
 
-print("countries.csv cleaned and saved")
+print("countries.csv cleaned and saved...")
 
 
 crops = pd.read_csv("DataSets_Raw/Crop_Yield_1.csv")
 
-print("Raw crop columns: ")
+print("Raw crop columns:- ")
 print(crops.columns)
 crops_clean = crops[["Crop","Yield","Year"]]
