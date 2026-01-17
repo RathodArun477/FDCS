98e59dc2f8fc72c663d9ec29ae5381202f6aa18a
aishwaryavollala <aishwaryavollala012@gmail.com>
2026-01-17 21:39:53 +0530

    modified 2

--- Analysis.py
+++ Analysis.py
@@ -1,15 +1,65 @@
-                                           import pandas as pd
+import pandas as pd
 
-df = pd.read_csv("DataSets_Loaded/countries.csv")
+df_temp=pd.read_csv("/content/drive/MyDrive/Major Project drive/countries.csv")
+df_crop=pd.read_csv("/content/drive/MyDrive/Major Project drive/crops.csv")
+df_totalproduction=pd.read_csv("/content/drive/MyDrive/Major Project drive/crops_country.csv")
+df_recipe=pd.read_csv("/content/drive/MyDrive/Major Project drive/recipes.csv")
+df_calories=pd.read_csv("/content/drive/MyDrive/Major Project drive/recipes_country.csv")
 
-df.columns = df.columns.str.strip().str.lower()
+country_name=input("Enter country name: ").strip().lower()
 
-country = input("Enter country name: ").strip().lower()
+df_temp['Country']=df_temp['Country'].str.strip().str.lower()
+df_crop['Country']=df_crop['Country'].str.strip().str.lower()
+df_totalproduction['Country']=df_totalproduction['Country'].str.strip().str.lower()
+df_recipe['Country']=df_recipe['Country'].str.strip().str.lower()
+df_calories['Country']=df_calories['Country'].str.strip().str.lower()
 
-result = df[df['country'].str.lower() == country]
+if country_name not in df_temp['Country'].values:
+  print("Country not found. ")
+  exit()
 
-if result.empty:
-    print("Country not found")
-else:
-    print("Climate:", result.iloc[0]['Temperature'])
-print("Hey")
+country_crops=df_crop[df_crop['Country']==country_name]
+
+total_crop_production=country_crops['Production'].sum()
+
+major_crops=(
+    country_crops.groupby("Crop")["Production"].sum()
+    .sort_values(ascending=False)
+    .head(5)
+)
+
+country_recipes=df_recipe[df_recipe['Country']==country_name]
+
+if country_recipes.empty:
+  print("No recipes found for",country_name)
+
+# country_recipes_calories=pd.merge(
+#     country_recipes,
+#     df_calories,
+#     left_on=["Country","Recipe Name"],
+#     right_on=["Country","Avg_Calories"],
+#     how="left"
+# )
+
+# print("\n")
+
+print(f"Total crop production in {country_name}: {total_crop_production}")
+print("\n")
+print("Major crops in",country_name,":")
+print("\n")
+print(major_crops)
+print("\n")
+print("Recipes in",country_name,":")
+print("\n")
+print(country_recipes[["Recipe Name","CaloriesPerServing"]])
+print("\n")
+# print("Calories in",country_name,":")
+# print("\n")
+# print(country_recipes_calories)
+# print("\n")
+
+
+
+
+
+
